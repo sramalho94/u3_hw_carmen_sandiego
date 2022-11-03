59006626ecd9b1989ac292b6b0f81a07218280af
Ben Manning <bmanning@me.com>
2022-11-03 11:38:41 -0400

    another modification to the hints

--- starter-code/clues.sql
+++ starter-code/clues.sql
@@ -36,13 +36,7 @@
 
 
 
--- Clue #7: She knows we're on to her – her taxi dropped her off at the international airport, and she beat us to
--- the boarding gates. We have one chance to catch her, we just have to know where she's heading and beat her to the
--- landing dock.
-
-
-
--- Clue #8:  She knows we're on to her – her taxi dropped her off at the international airport, and she beat us to
+-- Clue #7:  She knows we're on to her – her taxi dropped her off at the international airport, and she beat us to
 -- the boarding gates. We have one chance to catch her, we just have to know where she's heading and beat her to the
 -- landing dock. 
 -- Lucky for us, she's getting cocky. She left us a note, and I'm sure she thinks she's very clever, but
