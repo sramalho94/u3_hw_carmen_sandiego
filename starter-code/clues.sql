ac9bebd61a171d84e02f7e1587323cfc6e417890
Ben Manning <bmanning@me.com>
2022-11-03 11:36:35 -0400

    remove the seventh step

--- starter-code/clues.sql
+++ starter-code/clues.sql
@@ -18,9 +18,9 @@
 
 
 -- Clue #4: We're booking the first flight out – maybe we've actually got a chance to catch her this time.
- -- There are only two cities she could be flying to in the country. One is named the same as the country – that
- -- would be too obvious. We're following our gut on this one; find out what other city in that country she might
- --  be flying to.
+-- There are only two cities she could be flying to in the country. One is named the same as the country – that
+-- would be too obvious. We're following our gut on this one; find out what other city in that country she might
+--  be flying to.
 
 
 
@@ -31,18 +31,21 @@
 
 
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards
- -- the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll
- -- follow right behind you!
+-- the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll
+-- follow right behind you!
 
 
 
 -- Clue #7: She knows we're on to her – her taxi dropped her off at the international airport, and she beat us to
- -- the boarding gates. We have one chance to catch her, we just have to know where she's heading and beat her to the
- -- landing dock HINT:Look at number 8.
+-- the boarding gates. We have one chance to catch her, we just have to know where she's heading and beat her to the
+-- landing dock.
 
 
 
--- Clue #8: Lucky for us, she's getting cocky. She left us a note, and I'm sure she thinks she's very clever, but
+-- Clue #8:  She knows we're on to her – her taxi dropped her off at the international airport, and she beat us to
+-- the boarding gates. We have one chance to catch her, we just have to know where she's heading and beat her to the
+-- landing dock. 
+-- Lucky for us, she's getting cocky. She left us a note, and I'm sure she thinks she's very clever, but
 -- if we can crack it, we can finally put her where she belongs – behind bars.
 
 
